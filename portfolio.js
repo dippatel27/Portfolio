4723f802ad8aed12e21e00a4ea340cb694c05e77
dippatel27 <dipdhpatel27@gmail.com>
2022-01-24 23:57:37 +0530

    on click remove toggle

--- portfolio.js
+++ portfolio.js
@@ -20,20 +20,22 @@ $(document).ready(function() {
 
     // smooth scrolling 
 
-    $('a[href*="#"]').on('click', function(e) {
 
-        e.preventDefault();
+});
 
-        $('html, body').animate({
 
-                scrollTop: $($(this).attr('href')).offset().top,
+$('a[href*="#"]').on('click', function(e) {
 
-            },
-            500,
-            'linear'
-        );
+    e.preventDefault();
 
-    });
+    $('html, body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+        500,
+        'linear'
+    );
 
 });
 
